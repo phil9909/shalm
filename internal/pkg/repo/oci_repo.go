3774913c5365c151809f8e5bfe8f38dffe5135fd
Ulrich Kramer <u.kramer@sap.com>
2019-11-13 08:19:58 +0100

    Use oras
    Co-authored-by: Philipp Stehle <philipp.stehle@sap.com>

--- internal/pkg/repo/oci_repo.go
+++ internal/pkg/repo/oci_repo.go
@@ -2,18 +2,44 @@ package repo
 
 import (
 	"archive/tar"
+	"bytes"
+	"context"
 	"fmt"
 	"io"
 	"os"
 	"path"
 	"path/filepath"
+
+	"github.com/containerd/containerd/remotes"
+
+	"github.com/containerd/containerd/remotes/docker"
+	"github.com/deislabs/oras/pkg/content"
+	"github.com/deislabs/oras/pkg/oras"
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 )
 
 // OciRepo -
 type OciRepo struct {
-	BaseDir string
+	BaseDir  string
+	resolver remotes.Resolver
 }
 
+const (
+	customMediaType = "application/x-tar"
+)
+
+func NewOciRepo(basedir string) *OciRepo {
+	return &OciRepo{
+		BaseDir: basedir,
+		resolver: docker.NewResolver(docker.ResolverOptions{
+			Hosts: docker.ConfigureDefaultRegistries(
+				docker.WithAuthorizer(
+					docker.NewDockerAuthorizer(docker.WithAuthCreds(func(repository string) (s string, s2 string, e error) {
+						return "_json_key", os.Getenv("GCR_ADMIN_CREDENTIALS"), nil
+					})))),
+		}),
+	}
+}
 func (r *OciRepo) cacheDir() string {
 	return path.Join(r.BaseDir)
 }
@@ -37,24 +63,48 @@ func (r *OciRepo) Push(name string) error {
 	if err != nil {
 		return err
 	}
+	buffer := bytes.Buffer{}
 	os.MkdirAll(r.tarDir(), 0755)
-	tarFile := path.Join(r.tarDir(), name+".tar")
-	err = tarCreate(dir, tarFile)
+	err = tarCreate(dir, &buffer)
 	if err != nil {
 		return err
 	}
+	ref := "gcr.io/peripli/oras:test"
+
+	ctx := context.Background()
+
+	// Push file(s) w custom mediatype to registry
+	memoryStore := content.NewMemoryStore()
+	desc := memoryStore.Add(name, customMediaType, buffer.Bytes())
+	pushContents := []ocispec.Descriptor{desc}
+	desc, err = oras.Push(ctx, r.resolver, ref, memoryStore, pushContents)
+	if err != nil {
+		return err
+	}
+
+	return nil
+	// Pull file(s) from registry and save to disk
+	fmt.Printf("Pulling from %s and saving to %s...\n", ref, fileName)
+	fileStore := content.NewFileStore("")
+	defer fileStore.Close()
+	allowedMediaTypes := []string{customMediaType}
+	desc, _, err = oras.Pull(ctx, r.resolver, ref, fileStore, oras.WithAllowedMediaTypes(allowedMediaTypes))
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Pulled from %s with digest %s\n", ref, desc.Digest)
+	fmt.Printf("Try running 'cat %s'\n", fileName)
+	return nil
 	return err
 }
 
-func tarCreate(dir string, dest string) error {
-	out, err := os.Create(dest)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-	// gzip := gzip.NewWriter(out)
-	// defer gzip.Close()
-	tw := tar.NewWriter(out)
+// Pull -
+func (r *OciRepo) Pull(name string) error {
+	retunr nil
+}
+
+func tarCreate(dir string, writer io.Writer) error {
+	tw := tar.NewWriter(writer)
 	defer tw.Close()
 	return filepath.Walk(dir, func(file string, info os.FileInfo, err error) error {
 		if err != nil {
