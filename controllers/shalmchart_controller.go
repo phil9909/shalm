7f252bc0a67e44e540dfb0c8fa76cacd9b3e16e9
Ulrich Kramer <ulrich.kramer@web.de>
2019-12-24 13:41:43 +0100

    Implement finalizer for controller

--- controllers/shalmchart_controller.go
+++ controllers/shalmchart_controller.go
@@ -17,6 +17,7 @@ package controllers
 
 import (
 	"context"
+	"reflect"
 
 	"github.com/kramerul/shalm/pkg/shalm"
 	"go.starlark.net/starlark"
@@ -25,9 +26,12 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/event"
 
 	shalmv1a1 "github.com/kramerul/shalm/api/v1alpha1"
 )
+
+var myFinalizerName = "controller.shalm.kramerul.github.com"
 
 // ShalmChartReconciler reconciles a ShalmChart object
 type ShalmChartReconciler struct {
@@ -36,6 +40,9 @@ type ShalmChartReconciler struct {
 	Scheme *runtime.Scheme
 	Repo   shalm.Repo
 	K8s    func(kubeconfig string) shalm.K8s
+}
+
+type shalmChartPredicate struct {
 }
 
 // +kubebuilder:rbac:groups=shalm.kramerul.github.com,resources=shalmcharts,verbs=get;list;watch;create;update;patch;delete
@@ -52,20 +59,103 @@ func (r *ShalmChartReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) 
 	if err != nil {
 		return result, err
 	}
+	if shalmChart.ObjectMeta.DeletionTimestamp.IsZero() {
+		if !containsString(shalmChart.ObjectMeta.Finalizers, myFinalizerName) {
+			shalmChart.ObjectMeta.Finalizers = append(shalmChart.ObjectMeta.Finalizers, myFinalizerName)
+			if err := r.Update(context.Background(), &shalmChart); err != nil {
+				return result, err
+			}
+		}
+		return result, r.apply(&shalmChart.Spec)
+	}
+	if containsString(shalmChart.ObjectMeta.Finalizers, myFinalizerName) {
+		if err := r.delete(&shalmChart.Spec); err != nil {
+			return result, err
+		}
+
+		shalmChart.ObjectMeta.Finalizers = removeString(shalmChart.ObjectMeta.Finalizers, myFinalizerName)
+		if err := r.Update(context.Background(), &shalmChart); err != nil {
+			return result, err
+		}
+	}
+
+	return result, err
+
+}
+
+func (r *ShalmChartReconciler) apply(spec *shalmv1a1.ShalmChartSpec) error {
 	thread := &starlark.Thread{Name: "main"}
-	spec := &shalmChart.Spec
 	chart, err := r.Repo.GetFromGo(thread, spec.URL, spec.Namespace, false, spec.Args, spec.KwArgs)
 	if err != nil {
-		return result, err
+		return err
 	}
-	err = chart.Apply(thread, r.K8s(spec.KubeConfig))
+	return chart.Apply(thread, r.K8s(spec.KubeConfig))
+}
 
-	return result, nil
+func (r *ShalmChartReconciler) delete(spec *shalmv1a1.ShalmChartSpec) error {
+	thread := &starlark.Thread{Name: "main"}
+	chart, err := r.Repo.GetFromGo(thread, spec.URL, spec.Namespace, false, spec.Args, spec.KwArgs)
+	if err != nil {
+		return err
+	}
+	return chart.Delete(thread, r.K8s(spec.KubeConfig))
 }
 
 // SetupWithManager -
 func (r *ShalmChartReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&shalmv1a1.ShalmChart{}).
+		WithEventFilter(&shalmChartPredicate{}).
 		Complete(r)
+}
+
+// Create -
+func (r *shalmChartPredicate) Create(event.CreateEvent) bool {
+	return true
+}
+
+// Delete -
+func (r *shalmChartPredicate) Delete(event.DeleteEvent) bool {
+	return false
+}
+
+// Update -
+func (r *shalmChartPredicate) Update(ev event.UpdateEvent) bool {
+	old := ev.ObjectOld.(*shalmv1a1.ShalmChart)
+	new := ev.ObjectNew.(*shalmv1a1.ShalmChart)
+	if !reflect.DeepEqual(old.Spec, new.Spec) {
+		return true
+	}
+	if !containsString(new.ObjectMeta.Finalizers, myFinalizerName) {
+		return true
+	}
+	if !new.ObjectMeta.DeletionTimestamp.IsZero() {
+		return true
+	}
+	return false
+}
+
+// Generic -
+func (r *shalmChartPredicate) Generic(event.GenericEvent) bool {
+	return false
+}
+
+// Helper functions to check and remove string from a slice of strings.
+func containsString(slice []string, s string) bool {
+	for _, item := range slice {
+		if item == s {
+			return true
+		}
+	}
+	return false
+}
+
+func removeString(slice []string, s string) (result []string) {
+	for _, item := range slice {
+		if item == s {
+			continue
+		}
+		result = append(result, item)
+	}
+	return
 }
--- controllers/shalmchart_controller_test.go
+++ controllers/shalmchart_controller_test.go
@@ -12,9 +12,11 @@ import (
 	"path"
 	"path/filepath"
 	goruntime "runtime"
+	"time"
 
 	"github.com/kramerul/shalm/pkg/shalm"
 
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 
 	shalmv1a1 "github.com/kramerul/shalm/api/v1alpha1"
@@ -25,6 +27,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 var (
@@ -36,14 +39,22 @@ var (
 
 var _ = Describe("ShalmChartReconciler", func() {
 
+	var server http.Server
+	http.HandleFunc("/mariadb.tgz", func(w http.ResponseWriter, r *http.Request) {
+		content, _ := ioutil.ReadFile(path.Join(example, "mariadb-6.12.2.tgz"))
+		w.Write(content)
+	})
+
+	BeforeEach(func() {
+		server = http.Server{Addr: "127.0.0.1:8675", Handler: nil}
+		go server.ListenAndServe()
+	})
+
+	AfterEach(func() {
+		server.Shutdown(context.Background())
+	})
+
 	It("applies shalm chart correct", func() {
-
-		http.HandleFunc("/mariadb.tgz", func(w http.ResponseWriter, r *http.Request) {
-			content, _ := ioutil.ReadFile(path.Join(example, "mariadb-6.12.2.tgz"))
-			w.Write(content)
-		})
-
-		go http.ListenAndServe("127.0.0.1:8675", nil)
 
 		buffer := &bytes.Buffer{}
 		k8s := &FakeK8s{
@@ -54,24 +65,33 @@ var _ = Describe("ShalmChartReconciler", func() {
 		k8s.ForNamespaceStub = func(s string) shalm.K8s {
 			return k8s
 		}
+		chart := shalmv1a1.ShalmChart{
+			Spec: shalmv1a1.ShalmChartSpec{
+				Values:     nil,
+				Args:       nil,
+				KwArgs:     nil,
+				KubeConfig: "",
+				Namespace:  "",
+				URL:        "http://localhost:8675/mariadb.tgz",
+			},
+		}
+
 		client := &FakeClient{
 			GetStub: func(ctx context.Context, name types.NamespacedName, object runtime.Object) error {
 				switch object := object.(type) {
 				case *shalmv1a1.ShalmChart:
-					chart := shalmv1a1.ShalmChart{
-						Spec: shalmv1a1.ShalmChartSpec{
-							Values:     nil,
-							Args:       nil,
-							KwArgs:     nil,
-							KubeConfig: "",
-							Namespace:  "",
-							URL:        "http://localhost:8675/mariadb.tgz",
-						},
-					}
 					chart.DeepCopyInto(object)
 					return nil
 				}
 				return apierrors.NewNotFound(schema.GroupResource{}, name.String())
+			},
+			UpdateStub: func(ctx context.Context, object runtime.Object, options ...client.UpdateOption) error {
+				switch object := object.(type) {
+				case *shalmv1a1.ShalmChart:
+					object.DeepCopyInto(&chart)
+					return nil
+				}
+				return apierrors.NewNotFound(schema.GroupResource{}, "???")
 			},
 		}
 		reconciler := ShalmChartReconciler{
@@ -85,6 +105,68 @@ var _ = Describe("ShalmChartReconciler", func() {
 		}
 		_, err := reconciler.Reconcile(ctrl.Request{})
 		Expect(err).NotTo(HaveOccurred())
+		Expect(buffer.String()).To(ContainSubstring("serviceName: mariadb-master"))
+		Expect(chart.ObjectMeta.Finalizers).To(ContainElement("controller.shalm.kramerul.github.com"))
+		Expect(k8s.ApplyCallCount()).To(Equal(1))
+	})
+	It("deletes shalm chart correct", func() {
+
+		buffer := &bytes.Buffer{}
+		k8s := &FakeK8s{
+			DeleteStub: func(cb func(io.Writer) error, options *shalm.K8sOptions) error {
+				return cb(buffer)
+			},
+		}
+		k8s.ForNamespaceStub = func(s string) shalm.K8s {
+			return k8s
+		}
+		chart := shalmv1a1.ShalmChart{
+			ObjectMeta: v1.ObjectMeta{
+				Finalizers: []string{"controller.shalm.kramerul.github.com"},
+				DeletionTimestamp: &v1.
+					Time{Time: time.Now()},
+			},
+			Spec: shalmv1a1.ShalmChartSpec{
+				Values:     nil,
+				Args:       nil,
+				KwArgs:     nil,
+				KubeConfig: "",
+				Namespace:  "",
+				URL:        "http://localhost:8675/mariadb.tgz",
+			},
+		}
+
+		client := &FakeClient{
+			GetStub: func(ctx context.Context, name types.NamespacedName, object runtime.Object) error {
+				switch object := object.(type) {
+				case *shalmv1a1.ShalmChart:
+					chart.DeepCopyInto(object)
+					return nil
+				}
+				return apierrors.NewNotFound(schema.GroupResource{}, name.String())
+			},
+			UpdateStub: func(ctx context.Context, object runtime.Object, options ...client.UpdateOption) error {
+				switch object := object.(type) {
+				case *shalmv1a1.ShalmChart:
+					object.DeepCopyInto(&chart)
+					return nil
+				}
+				return apierrors.NewNotFound(schema.GroupResource{}, "???")
+			},
+		}
+		reconciler := ShalmChartReconciler{
+			Client: client,
+			Log:    ctrl.Log.WithName("reconciler"),
+			Scheme: nil,
+			Repo:   shalm.NewRepo(),
+			K8s: func(kubeconfig string) shalm.K8s {
+				return k8s
+			},
+		}
+		_, err := reconciler.Reconcile(ctrl.Request{})
+		Expect(err).NotTo(HaveOccurred())
+		Expect(chart.ObjectMeta.Finalizers).NotTo(ContainElement("controller.shalm.kramerul.github.com"))
+		Expect(k8s.DeleteCallCount()).To(Equal(1))
 		Expect(buffer.String()).To(ContainSubstring("serviceName: mariadb-master"))
 	})
 })
